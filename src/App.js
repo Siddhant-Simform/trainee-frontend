ca6ba65e0ed23fa6170bd768bbdd8d230e8f8f27
Siddhant-Simform <siddhant.rathod@simformsolutions.com>
2026-05-01 16:41:52 +0530

    fix: remove unused useState import causing CI build failure

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
 import './App.css';
 import GetEmployeesPage from './pages/GetEmployeesPage';
